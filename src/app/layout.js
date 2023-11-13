7169d55085c59c4daa54efc5c332538551e2aebf
Kibieptr <138297239+Lynnxha@users.noreply.github.com>
2023-11-13 08:46:35 +0700

    update: dependencies

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/utilities/Navbar";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
