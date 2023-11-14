c0f88aea8811f620c349ece6b89d5083634564ad
Kibieptr <138297239+Lynnxha@users.noreply.github.com>
2023-11-14 19:24:32 +0700

    add: theme & loading

---
+++ src/app/popular/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Page = () => {
+    return (
+        <div>Popular Page</div>
+    )
+}
+
+export default Page
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,26 @@
 import Link from "next/link";
 import InputSearch from "./InputSearch";
+import Image from "next/image";
 
 const Navbar = () => {
     return (
         <header className="bg-color-oceandark">
             <div className="flex md:flex-row flex-col justify-between md:items-center gap-3 p-4">
-                <Link href="/" className="font-bold text-white text-2xl text-color-primary">Lynn Anime</Link>
+                <Link href="/" className="font-bold text-white text-2xl text-color-primary"> Lynn Anime List
+                    {/* <div className="flex items-center">
+                        <Image
+                            src="/favicon.ico"
+                            alt="Lynn Anime Logo"
+                            width={40}
+                            height={40}
+                        />
+                        <span className="font-bold text-2xl text-color-primary">Lynn Anime</span>
+                    </div> */}
+                </Link>
                 <InputSearch />
             </div>
         </header>
-    )
+    );
 }
 
-export default Navbar;
+export default Navbar
