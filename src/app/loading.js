07c32e3e325fe1dde6b296e593961ba822cc54a5
Kibieptr <138297239+Lynnxha@users.noreply.github.com>
2023-11-15 09:23:22 +0700

    fixed: all

--- src/app/loading.js
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-    return (
-        <div className="flex justify-center items-center min-h-screen">
-            <div className="loader"></div>
-        </div>
-    )
-}
-
-export default Loading
--- src/app/popular/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Page = () => {
-    return (
-        <div>Popular Page</div>
-    )
-}
-
-export default Page
