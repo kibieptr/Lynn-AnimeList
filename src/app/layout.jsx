b80716a9a73c8691dd511a6cd8312b700c02a7c4
Kibieptr <138297239+Lynnxha@users.noreply.github.com>
2023-12-11 05:22:30 +0700

    fix feat: seo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,13 +7,14 @@ const gabarito = Gabarito({ subsets: ["latin"] });
 export const metadata = {
   title: "Lynn Anime - Indonesian Anime and Manga Hub",
   description: "Lynn Anime - Indonesian Anime and Manga Hub Website. Discover the pulse of Indonesian anime and manga at Lynn Anime, your go-to online community. Dive into the latest releases, engaging discussions, and a vibrant fan base. Join us now! created by Kibieptr",
-  keywords: "Anime, Lynnanimelist, Anime News, Manga, Updated Anime Sites, ",
+  keywords: "Anime, Lynnanimelist, Anime News, Manga, Updated Anime Sites",
+  author: "Kibieptr",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${gabarito.className} bg-color-dark`} suppressHydrationWarning={true}>z
+      <body className={`${gabarito.className} bg-color-dark`} suppressHydrationWarning={true}>
         <Navbar />
         {children}
       </body>
